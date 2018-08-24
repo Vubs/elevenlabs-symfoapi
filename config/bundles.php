2904ccb36b03cb4fb60d8164218e491db147b94a
Vincent Bertrand <b.vincent.iim@gmail.com>
2018-08-24 16:35:28 +0200

    added nelmio cors for the request coming from the react app

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
